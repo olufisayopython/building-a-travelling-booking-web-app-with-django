bc0a34fcece3c7af8a45836c00ca4766ea1e2408
Olubode Olufisayo <61015847+olufisayopython@users.noreply.github.com>
2023-03-03 17:55:12 +0100

    Add files via upload

---
+++ estate/views.py
@@ -0,0 +1,60 @@
+from django.shortcuts import render,redirect
+from .models import destination
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
+
+# Create your views here.
+
+def index(request):
+    dests = destination.objects.all()
+
+    return render(request, 'index.html', {'dests': dests})
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request,'wrong username or password')
+            return redirect('login')
+
+    else:
+        return render(request, 'login.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+
+        if (password1==password2):
+            if User.objects.filter(username=username).exists():
+                messages.info(request, 'username taken')
+                return redirect('register')
+            elif User.objects.filter(email=email).exists():
+                messages.info(request, 'email taken')
+                return redirect('register')
+            else:
+                user = User.objects.create_user(username=username, password=password1, email=email, first_name=first_name, lastname=last_name)
+                user.save();
+                return redirect('login')
+        else:
+            messages.info(request, 'Password did not match')
+            return redirect('register')
+        return redirect('/')
+    else:
+        return render(request, 'register.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('login')
